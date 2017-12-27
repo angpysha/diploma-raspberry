37107ddfa48e838b679c1cf1600752d213fea0b9
Andrew Petrowsky <andrew.petrowsky@outlook.com>
2017-12-27 22:22:31 +0200

    Add backgroud worker to DHT11

--- src/main/java/com/andrewpetrowski/diploma/raspberry/MainClass.java
+++ src/main/java/com/andrewpetrowski/diploma/raspberry/MainClass.java
@@ -1,17 +1,36 @@
 package com.andrewpetrowski.diploma.raspberry;
 
+import com.andrewpetrowski.diploma.bridgelib.Models.DHT11_Data;
+import com.andrewpetrowski.diploma.raspberry.BackgroundTasks.DHTRunner;
+import com.andrewpetrowski.diploma.raspberry.BackgroundTasks.IDHTCallback;
 import com.andrewpetrowski.diploma.raspberry.Sensors.DHT11;
-import com.pi4j.io.gpio.RaspiPin;
-import com.pi4j.wiringpi.Gpio;
 
-public class MainClass {
+/**
+ * Main class for program
+ * @version 1.0
+ * @author Andrew Petrowsky
+ */
+public class MainClass implements IDHTCallback{
 
     public static void main(String[] args) throws InterruptedException {
-        DHT11 dht11 = new DHT11();
-        while(true)
-        {  Thread.sleep(1000);
-            dht11.getTemperature(7);
+
+        MainClass mainClass = new MainClass();
+        mainClass.start();
 
         }
+
+
+    public void start() {
+        DHTRunner dhtRunner = new DHTRunner(3600,this);
+        while (true){
+
+        }
+    }
+
+    @Override
+    public void acceptDHT(DHT11_Data dht11_data) {
+        System.out.println(String.format("Temperature: %f; Humidity: %f"
+                ,dht11_data.getTemperature()
+                ,dht11_data.getHumidity()));
     }
 }

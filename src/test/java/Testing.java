9349e62d72575288df63ef257bcacb22c3b0969b
Andrew Petrowsky <andrew.petrowsky@outlook.com>
2017-12-31 11:37:01 +0200

    Testing

---
+++ src/test/java/Testing.java
@@ -0,0 +1,25 @@
+import com.andrewpetrowski.diploma.raspberry.BackgroundTasks.DHTRunner;
+import com.andrewpetrowski.diploma.raspberry.Sensors.DHT11;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Testing {
+    @Test
+    public void TestInterval() {
+          //  DHTRunner runner = new DHTRunner(15,this);
+            System.out.println("aaaaaaaa");
+//        while (true) {
+//
+//        }
+    }
+
+    @Test
+    public void TestSensor() {
+        DHT11 dht11 = new DHT11();
+
+        float[] da = dht11.readData(7);
+        Assert.assertTrue(da[0]>0);
+        Assert.assertTrue(da[1]>0);
+    }
+
+}

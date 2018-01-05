0722d00d61ee1b2cea1e0099ee696065f8e9763c
Andrew Petrowsky <andrew.petrowsky@outlook.com>
2018-01-05 20:47:17 +0200

    Set command line

--- src/main/java/com/andrewpetrowski/diploma/raspberry/MainClass.java
+++ src/main/java/com/andrewpetrowski/diploma/raspberry/MainClass.java
@@ -5,6 +5,9 @@ import com.andrewpetrowski.diploma.raspberry.BackgroundTasks.DHTRunner;
 import com.andrewpetrowski.diploma.raspberry.BackgroundTasks.IDHTCallback;
 import com.andrewpetrowski.diploma.raspberry.Sensors.DHT11;
 
+import java.text.SimpleDateFormat;
+import java.util.logging.SimpleFormatter;
+
 /**
  * Main class for program
  * @version 1.0
@@ -12,6 +15,7 @@ import com.andrewpetrowski.diploma.raspberry.Sensors.DHT11;
  */
 public class MainClass implements IDHTCallback{
 
+    private SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     public static void main(String[] args) throws InterruptedException {
 
         MainClass mainClass = new MainClass();
@@ -29,7 +33,8 @@ public class MainClass implements IDHTCallback{
 
     @Override
     public void acceptDHT(DHT11_Data dht11_data) {
-        System.out.println(String.format("Temperature: %f; Humidity: %f"
+        System.out.println(String.format("%s: Temperature: %f; Humidity: %f"
+                ,df.format(dht11_data.getCreated_at())
                 ,dht11_data.getTemperature()
                 ,dht11_data.getHumidity()));
     }

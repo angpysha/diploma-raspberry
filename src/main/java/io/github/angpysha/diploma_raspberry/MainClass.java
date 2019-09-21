f02374e9178fa345cfa7531e7d67979f6419b258
Andrii Petrovskyi <angpysha@outlook.com>
2019-09-21 16:17:42 +0300

    Dynamic lbrary loading

--- src/main/java/io/github/angpysha/diploma_raspberry/MainClass.java
+++ src/main/java/io/github/angpysha/diploma_raspberry/MainClass.java
@@ -8,6 +8,13 @@ import io.github.angpysha.diploma_raspberry.BackgroundTasks.IBMPCallback;
 import io.github.angpysha.diploma_raspberry.BackgroundTasks.IDHTCallback;
 import io.github.angpysha.diploma_raspberry.Socket.Socket;
 
+import java.io.Console;
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.text.SimpleDateFormat;
 
 /**
@@ -28,14 +35,73 @@ public class MainClass implements IDHTCallback,IBMPCallback{
 
     public void start() {
        // BMP180 bmp180 = new BMP180();
-        AppConfig appConfig = AppConfig.getInstanse("appconfig.ini");
-        Socket socket = Socket.getInstanse(appConfig.getSocketUrl());
-        socket.socketio.connect();
-        DHTRunner dhtRunner = new DHTRunner(3600,this);
-        BMPRunner bmpRunner = new BMPRunner(3600,this);
+//        AppConfig appConfig = AppConfig.getInstanse("appconfig.ini");
+//        Socket socket = Socket.getInstanse(appConfig.getSocketUrl());
+//        socket.socketio.connect();
+//        DHTRunner dhtRunner = new DHTRunner(3600,this);
+//        BMPRunner bmpRunner = new BMPRunner(3600,this);
+        File libsDir = new File("./libs");
+        File[] files = libsDir.listFiles();
+
+        for (File file1:files) {
+            String ext = getFileExtension(file1);
+            if (ext.contains("so")) {
+                String filename = file1.getAbsolutePath();
+                System.out.println("Load nativelib: " +filename);
+              //  System.load(filename);
+            }
+        }
+
+        for (File file:files) {
+           // System.out.println(file.getName());
+            String ext = getFileExtension(file);
+           // System.out.println(ext);
+            if (ext.contains("jar")) {
+                try {
+                    System.out.println("Enter to dynamic loader");
+                    ClassLoader classLoader = new URLClassLoader(new URL[]{file.toURI().toURL()},
+                            getClass().getClassLoader());
+                    System.out.println(classLoader.toString());
+
+                    String name = file.getName().replace(".jar", "");
+                    System.out.println(name);
+                    Class<?> clazz = Class.forName("io.github.angpysha.raspberry." + name + ".Launcher", true, classLoader);
+                    System.out.println(clazz.toString());
+                    Method method = clazz.getDeclaredMethod("DoOperation");
+                    Object instanse = clazz.newInstance();
+                    Object meth = method.invoke(instanse);
+                    String str = (String)meth;
+                    System.out.println(str);
+                    //System.out.println(meth.hashCode());
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                } catch (NoSuchMethodException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (InstantiationException e) {
+                    e.printStackTrace();
+                } catch (InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            }
+//            } else if (ext.contains("so")) {
+//                String filename = file.getAbsolutePath();
+//                System.load(filename);
+//            }
+        }
         while (true){
          //   bmp180.Testing();
         }
+    }
+
+    private String getFileExtension(File file) {
+        String fileName = file.getName();
+        if(fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0)
+            return fileName.substring(fileName.lastIndexOf(".")+1);
+        else return "";
     }
 
     @Override
--- src/main/java/io/github/angpysha/diploma_raspberry/Sensors/DHT11.java
+++ src/main/java/io/github/angpysha/diploma_raspberry/Sensors/DHT11.java
@@ -1,8 +1,6 @@
 package io.github.angpysha.diploma_raspberry.Sensors;
 
 import io.github.angpysha.diploma_bridge.Models.DHT11_Data;
-import com.pi4j.wiringpi.Gpio;
-import com.pi4j.wiringpi.GpioUtil;
 
 /**
  * Gets DHT11 sensors data
@@ -40,91 +38,17 @@ public class DHT11 {
      */
     public DHT11() {
 
-        // setup wiringPi
-        if (Gpio.wiringPiSetup() == -1) {
-            System.out.println(" ==>> GPIO SETUP FAILED");
-            return;
-        }
+//        // setup wiringPi
+//        if (Gpio.wiringPiSetup() == -1) {
+//            System.out.println(" ==>> GPIO SETUP FAILED");
+//            return;
+//        }
 
 
 
       //  GpioUtil.export(7, GpioUtil.DIRECTION_OUT);
     }
 
-    /**
-     * Get sensor data
-     * @deprecated This method is much slower, then native JNI method.
-     * And not recommend for using.
-     * @param pin pin number
-     */
-    @Deprecated
-    public Float[] getData(final int pin) {
-        int laststate = Gpio.HIGH;
-
-        int j = 0,i;
-        dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
-
-        Gpio.pinMode(pin, Gpio.OUTPUT);
-        Gpio.digitalWrite(pin, Gpio.LOW);
-        Gpio.delay(20);
-
-        Gpio.digitalWrite(pin, Gpio.HIGH);
-        Gpio.delayMicroseconds(40);
-        Gpio.pinMode(pin, Gpio.INPUT);
-
-        Gpio.delayMicroseconds(10);
-        for (i = 0; i < MAXTIMINGS; i++) {
-            int counter = 0;
-            while (Gpio.digitalRead(pin) == laststate) {
-                counter++;
-                Gpio.delayMicroseconds(1);
-                if (counter == 255) {
-                    break;
-                }
-            }
-
-            laststate = Gpio.digitalRead(pin);
-          //  System.out.print(laststate);
-            if (counter == 255) {
-                break;
-            }
-
-            /* ignore first 3 transitions */
-            if ((i >= 4) && (i % 2 == 0)) {
-                /* shove each bit into the storage bytes */
-                dht11_dat[j / 8] <<= 1;
-                if (counter > 50) {
-                    dht11_dat[j / 8] |= 1;
-                }
-                j++;
-            }
-        }
-        // check we read 40 bits (8bit x 5 ) + verify checksum in the last
-        // byte
-        if (j >= 40 && checkParity()) {
-            float h = (float) ((dht11_dat[0] << 8) + dht11_dat[1]) / 10;
-            if (h > 100) {
-                h = dht11_dat[0]; // for DHT11
-            }
-            float c = (float) (((dht11_dat[2] & 0x7F) << 8) + dht11_dat[3]) / 10;
-            if (c > 125) {
-                c = dht11_dat[2]; // for DHT11
-            }
-            if ((dht11_dat[2] & 0x80) != 0) {
-                c = -c;
-            }
-            final float f = c * 1.8f + 32;
-            Float[] data = new Float[2];
-            data[0] = c;
-            data[1]=h;
-            return data;
-            //System.out.println("Humidity = " + h + " Temperature = " + c + "(" + f + "f)");
-        } else {
-            return new Float[2];
-            //System.out.println("Data not good, skip");
-        }
-
-    }
 
     /**
      * Check for data validity
